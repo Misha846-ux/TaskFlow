b952e9fe7a738faa8b619117c1785abb425c6f26
Ruslan Opihaylenko <rysya08shkolnik@gmail.com>
2026-02-23 10:59:26 +0200

    add UserDTOs

---
+++ TaskFlow.Application/DTOs/UserDTOs/UserGetDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskFlow.Application.DTOs.UserDTOs
+{
+    public class UserGetDto
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string GlobalRole { get; set; }
+        public string PassTolcon {  get; set; }
+        public string GreatedAt { get; set; }
+        public byte[] CompaniesId { get; set; }
+    }
+}
---
+++ TaskFlow.Application/DTOs/UserDTOs/UserPostDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskFlow.Application.DTOs.UserDTOs
+{
+    public class UserPostDto
+    {
+        public string UserName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public string PassTolcon {  get; set; }
+    }
+}
