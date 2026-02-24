736e9aa35680c4c68ebedb4486aad18c5d71952f
unknown <danakutumov@gmail.com>
2026-02-24 16:27:34 +0200

    Adding_TaskDtos

---
+++ TaskFlow.Application/DTOs/TaskDTOs/TaskGetDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskFlow.Domain.Enums.TasksEnums;
+
+namespace TaskFlow.Application.DTOs.TaskDTOs;
+
+public class TaskGetDto
+{
+    public int Id { get; set; }
+    public TaskStatus Status { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public TaskPriority Priority { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime DueDate { get; set; }
+    public int ProjectId { get; set; }
+    public int UserId { get; set; }
+}
---
+++ TaskFlow.Domain/Enums/TaskEnums/TaskStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskFlow.Domain.Enums.TaskEnums;
+
+internal enum TaskStatus
+{
+    Waiting = 0,
+    InProgress = 1,
+    Completed = 2,
+}
