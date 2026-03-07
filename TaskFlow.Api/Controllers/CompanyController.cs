0fd3bf700d59aa2eea4100905c3fd2effb39e99b
unknown <danakutumov@gmail.com>
2026-03-07 17:41:54 +0200

    FIXINGadding_company_controller

--- TaskFlow.Api/Controllers/CompanyController.cs
+++ TaskFlow.Api/Controllers/CompanyController.cs
@@ -1,6 +1,35 @@
-﻿namespace TaskFlow.Api.Controllers
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskFlow.Api.Controllers;
+
+[ApiController]
+[Route("/[controller]")]
+[Authorize(Roles = "Admin")]
+public class CompanyController : ControllerBase
 {
-    public class CompanyController
+    [HttpGet]
+    public async Task<IActionResult> GetAllCompanies()
     {
+        return Ok();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetCompanyById([FromRoute]int id)
+    {
+        return Ok();
+    }
+
+    [HttpDelete("{id}")]
+    [Authorize(Roles = "Admin")]
+    public async Task<IActionResult> DeleteCompanyById([FromRoute] int id)
+    {
+        return Ok();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateCompany([FromBody] CompanyPostDto company)
+    {
+            return Ok();
     }
 }
