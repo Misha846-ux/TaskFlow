15a2927c3113a2ec121ad4cff55aec9867d37109
Ruslan Opihaylenko <rysya08shkolnik@gmail.com>
2026-04-29 17:26:23 +0300

    Controller commit

--- TaskFlow.Api/Controllers/ProjectsController.cs
+++ TaskFlow.Api/Controllers/ProjectsController.cs
@@ -175,7 +175,6 @@ namespace TaskFlow.Api.Controllers
         /// <param name="cancellationToken"></param>
         /// <returns></returns>
         [HttpGet("by-company/{companyId}")]
-        [Authorize(Roles = "Admin")]
         public async Task<IActionResult> GetByCompanyId(int companyId, CancellationToken cancellationToken)
         {
             var projects = await _service.GetProjectsByCompanyIdAsync(companyId, cancellationToken);
@@ -191,7 +190,6 @@ namespace TaskFlow.Api.Controllers
         /// <param name="cancellationToken"></param>
         /// <returns></returns>
         [HttpGet("by-company/{companyId}/filtered")]
-        [Authorize(Roles = "Admin")]
         public async Task<IActionResult> GetByCompanyIdPagination(int companyId, [FromQuery] int count, [FromQuery] int side, CancellationToken cancellationToken)
         {
             var projects = await _service.GetProjectsByCompanyIdPaginationAsync(companyId, count, side, cancellationToken);
